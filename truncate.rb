b25003e733e75a706548fafd158f775509302df6
rem <kremartem@gmail.com>
2022-07-26 20:32:32 +0300

    done

--- truncate.rb
+++ truncate.rb
@@ -1,2 +1,15 @@
 module Truncate
+
+  def truncate(attribute, **options)
+   truncate_data(attribute, **options)
+  end
+
+  private
+
+    def truncate_data(attribute,  **options)
+      define_method "#{attribute}=" do |value|
+        result = value.length > options[:length] ? "#{value[0...options[:length]]}..." : value
+        instance_variable_set("@#{attribute}", result)
+      end
+    end
 end
